acad84dc7bc15d429345f12508a3aef21d32367a
Yann Oberson <yaoberso@c2r5s5.42lausanne.ch>
2024-10-09 14:18:07 +0200

    09oct

---
+++ libft/ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaoberso <yaoberso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 15:12:04 by yaoberso          #+#    #+#             */
+/*   Updated: 2024/10/04 10:23:43 by yaoberso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (*(unsigned char *)(s + i) == ((unsigned char)c))
+		{
+			return ((void *)(s + i));
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ libft/ft_memcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaoberso <yaoberso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 13:54:57 by yaoberso          #+#    #+#             */
+/*   Updated: 2024/10/08 13:09:48 by yaoberso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*s;
+	unsigned char	*d;
+
+	i = 0;
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	if (d == NULL && s == NULL)
+	{
+		return (NULL);
+	}
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
